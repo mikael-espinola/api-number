3ebee387868d905cea67978be819d18b07492e72
mikael-espinola <mikaelespinolaa@gmail.com>
2024-10-23 21:22:56 -0300

    Habilitando CORS

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from flask import Flask, jsonify
+from flask_cors import CORS
 import random
 
 app = Flask(__name__)
+CORS(app) 
 
 @app.route('/api/sort-number', methods=['GET'])
 def get_sorted_numbers():
@@ -10,4 +12,4 @@ def get_sorted_numbers():
     return jsonify(sorted_numbers)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
