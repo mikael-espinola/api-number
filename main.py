1257d13ee7a5b3419218c28b842cda1ce7326ac3
mikael-espinola <mikaelespinolaa@gmail.com>
2024-10-23 21:29:43 -0300

    Desabilitando CORS

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 from flask import Flask, jsonify
-from flask_cors import CORS
 import random
 
 app = Flask(__name__)
-CORS(app) 
 
 @app.route('/api/sort-number', methods=['GET'])
 def get_sorted_numbers():
@@ -12,4 +10,4 @@ def get_sorted_numbers():
     return jsonify(sorted_numbers)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
